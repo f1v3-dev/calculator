3b859505941ac463c0075d92bb7b36582bc9edcc
xotn3170 <xotn3170@gmail.com>
2023-08-28 18:47:54 +0900

    #2023-gitProject/4 multi() 구현

--- main.c
+++ main.c
@@ -35,7 +35,6 @@ int main(int argc, char* argv[])
 
 int add(int number1, int number2)
 {
-    //TODO implementation
     return number1 + number2s;
 }
 
@@ -46,8 +45,7 @@ int sub(int number1, int number2)
 
 int multi(int number1, int number2)
 {
-    //TODO implementation
-    return 0;
+    return number1 * number2;
 }
 
 int div(int number1, int number2)
